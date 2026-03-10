fef95da925404bfedb7952511221f0823029f814
Argeu Souza <argeusousaptbr@gmail.com>
2026-03-09 23:59:23 -0300

    feat: script.js

--- script.js
+++ script.js
@@ -1,15 +1,74 @@
-let display = document.getElementById("display");
-let valorInput = "";
-let operadorAtual = "";
+let display = document.getElementById('display');
+let numeroAtual = display.value;
+let numeroAnterior;
+let operadorAtual = '';
 
-function numeroDigitado(valor) {
-    valorInput += valor;
-    display.textContent = valorInput;
+function limpar() {
+    document.getElementById('display').value = '';
+}
+
+function numeroDigitado(numero) {
+    display.value += numero;
 }
 
 function operadorDigitado(operador) {
-    if(valorInput === "" && operadorAtual !== ".") return 
-        valorInput += operadorAtual;
-        display.textContent = valorInput;
-    
+    numeroAtual = display.value;
+    if (numeroAtual == '') {
+        return;
+    }
+    var ultimo = numeroAtual.charAt(numeroAtual.length - 1);
+    if (ultimo == "+" || ultimo == "-" || ultimo == "*" || ultimo == "/") {
+        return;
+    }
+
+    if (operadorAtual != '') {
+        calcular();
+    }
+
+    numeroAnterior = Number(numeroAtual);
+    operadorAtual = operador;
+
+    display.value = numeroAtual + operador;
+}
+
+function deletar() {
+    numeroAtual = display.value;
+    display.value = numeroAtual.substring(0, numeroAtual.length - 1);
+}
+
+function calcular() {
+    var expressao = display.value;
+    var partes = expressao.split(operadorAtual);
+
+    if (expressao == '') {
+        limpar();
+    }
+
+    numeroAnterior = Number(partes[0]);
+    numeroAtual = Number(partes[1]);
+
+    let resultado;
+
+    switch (operadorAtual) {
+        case '+':
+            resultado = numeroAnterior + numeroAtual;
+            break;
+        
+        case '-':
+            resultado = numeroAnterior - numeroAtual;
+            break;
+        
+        case '*':
+            resultado = numeroAnterior * numeroAtual;
+            break;
+        case '/':
+            resultado = numeroAnterior / numeroAtual;
+            break;
+        
+        default:
+            return;
+    }
+
+    display.value = resultado;
+    numeroAnterior = resultado;
 }
