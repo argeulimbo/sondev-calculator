9ab8a656cfe0e121c4a36ad9e4f18af35f3590ec
Argeu Souza <argeusousaptbr@gmail.com>
2026-03-10 13:28:56 -0300

    fix: correção do método de calcular ignorando regra matemática

--- script.js
+++ script.js
@@ -58,19 +58,38 @@ function operadorDigitado(operador) {
 
 function calcular() {
     let expressao = display.value;
-    let numeros = expressao.split(/[\+\-\*\/]/);
-    let operadores = expressao.split(/[0-9]+/).filter(op => op != '');
+    let numeros = expressao.split(/[\+\-\*\/]/).map(Number);
+    let operadores = expressao.split(/[0-9]+/).filter(op => op != "");
 
-    let resultado = parseFloat(numeros[0]);
-
-    for (let i=0; i<operadores.length; i++) {
+    for (let i=0; i < operadores.length; i++) {
         if (operadores[i] == '*' || operadores[i] == '/') {
+            let resultado; 
             if (operadores[i] == '*') {
-                resultado *= numero;
+                resultado = numeros[i] * numeros[i + i];
             }
-            if (o)
+            if (operadores[i] == '/') {
+                if (numeros[i +1] == 0) {
+                    display.value = 'Erro';
+                    return;
+                }
+                resultado = numeros[i] / numeros[i + 1];
+            }
+
+            numeros.splice(i, 2, resultado);
+            operadores.splice(i, 1);
+            i --;
         }
     }
+    let resultadoFinal = numeros[0];
+    for (let i=0; i<operadores.length; i++) {
+        if (operadores[i] == '+') {
+            resultadoFinal += numeros[i + 1];
+        }
+        if (operadores[i] == '-') {
+            resultadoFinal -= numeros[i + 1];
+        }
+    }
+    display.value = resultadoFinal;
 }
 
 function apagar() {
