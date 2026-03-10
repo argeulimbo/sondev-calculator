8db5ffce4ef1e62285bc33b8164258f56de2e5dc
Argeu Souza <argeusousaptbr@gmail.com>
2026-03-10 15:13:10 -0300

    refact: refatoração

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ function limpar() {
     display.value = '';
 }
 
+function apagar() {
+    let currentDisplay = display.value;
+    display.value = currentDisplay.substring(0, currentDisplay.length -1);
+}
+
 function numeroDigitado(numero) {
     display.value += numero;
 }
@@ -22,40 +27,6 @@ function operadorDigitado(operador) {
     display.value += operador;
 }
 
-/* function calcular() {
-    let expressao = display.value;
-    let numeros = expressao.split(/[\+\-\*\/]/);
-    let operadores = expressao.split(/[0-9]+/).filter(op => op != "");
-
-    let resultado = parseFloat(numeros[0]);
-
-    for (let i=0; i < operadores.length; i++) {
-        let numero = parseFloat(numeros[i + 1]);
-
-        // Adição
-        if (operadores[i] == '+') {
-            resultado += numero;
-        }
-
-        // Subtração
-        if (operadores[i] == '-') {
-            resultado -= numero;
-        }
-
-        // Multiplicação
-        if (operadores[i] == '*') {
-            resultado *= numero;
-        }
-
-        // Divisão
-        if (operadores[i] == '/') {
-            resultado /= numero;
-        }
-    }
-    display.value = resultado;
-}
-*/
-
 function calcular() {
     let expressao = display.value;
     let numeros = expressao.split(/[\+\-\*\/]/).map(Number);
@@ -73,7 +44,7 @@ function calcular() {
             }
             if (operadores[i] == '/') {
                 if (numeros[i +1] == 0) {
-                    display.value = 'Erro';
+                    display.value = 'Erro';  // Divisão impossível X / 0 = Erro
                     return;
                 }
                 resultado = numeros[i] / numeros[i + 1];
@@ -96,7 +67,3 @@ function calcular() {
     display.value = resultadoFinal;
 }
 
-function apagar() {
-    let currentDisplay = display.value;
-    display.value = currentDisplay.substring(0, currentDisplay.length -1);
-}
